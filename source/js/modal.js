ed62c531f8fbeea35d79b17e522c00fd5436e23f
Konstantin <localhost@local.domain>
2020-08-27 03:01:18 +0300

    Remake catalog; some fixes

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,10 +1,14 @@
-var modalOpen = document.querySelector(".product-week__cart-link");
+var modalOpen = document.querySelectorAll(".product-week__cart-link, .card__cart-link");
 var modalPopup = document.querySelector(".modal");
 
-modalOpen.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  modalPopup.classList.add("modal--show");
+
+modalOpen.forEach(function (link) {
+  link.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modalPopup.classList.add("modal--show");
+  })
 });
+
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
